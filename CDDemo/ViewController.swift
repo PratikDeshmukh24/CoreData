582f140e912d65567d0216fe5036f343174f5c6a
Pratik Prabhakar Deshmukh <pratideshmukh@deloitte.com>
2021-10-23 12:04:27 +0530

    Update Data func
    done

--- CDDemo/ViewController.swift
+++ CDDemo/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createData()
+        fetchData()
     }
 
+    
+    //MARK: Core Data Methods
+    private let entityName = "Users"
+    private func createData() {
+        guard let managedContext = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {return}
+        
+        guard let userEntity = NSEntityDescription.entity(forEntityName: entityName, in: managedContext) else {
+            fatalError()
+        }
+        
+        let user = NSManagedObject(entity: userEntity, insertInto: managedContext)
+        user.setValue("pratik@gmail.com", forKey: "email")
+        user.setValue("1234", forKey: "password")
+        user.setValue("Pratik2", forKey: "username")
+        
+        do {
+            try managedContext.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func fetchData() {
+        guard let managedContext = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {return}
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+       // fetchRequest.predicate = NSPredicate(format: "username = %@", "Pratik1")
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            for user in result as! [NSManagedObject] {
+                let name = user.value(forKey: "username") as! String
+                print(name)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func UpdateData() {
+        guard let managedContext = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext else {return}
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+       // fetchRequest.predicate = NSPredicate(format: "username = %@", "Pratik1")
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            for user in result as! [NSManagedObject] {
+                let name = user.value(forKey: "username") as! String
+                print(name)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
